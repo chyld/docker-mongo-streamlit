8d375d22a0f0d5ce43a9d348e3a601519e44701c
Chyld Medford <chyld.medford@gmail.com>
2022-11-30 14:33:26 -0800

    changed events to pull model

--- alpha/main.py
+++ alpha/main.py
@@ -1,4 +1,5 @@
 import os
+import time
 import docker
 from pymongo import MongoClient
 from datetime import datetime
@@ -10,9 +11,15 @@ dclient = docker.DockerClient()
 mclient = MongoClient(mongo_uri)
 events = mclient.dashboards.container_events
 
-for event in dclient.events(decode=True):
-    receive_time = datetime.now()
-    megaevent = event | {'hostname': hostname, 'receive_time': receive_time}
-    print(f'Received event @ {receive_time}')
-    events.insert_one(megaevent)
+try:
+    while True:
+        for c in dclient.containers.list(all=True):
+            receive_time = datetime.now()
+            attrs = c.attrs
+            data = attrs | {'hostname': hostname, 'receive_time': receive_time}
+            events.insert_one(data)
+            print(f'Data inserted @ {receive_time}')
+        time.sleep(10)    
+except:
+    print('An error occurred')
 
--- alpha/run.sh
+++ alpha/run.sh
@@ -5,20 +5,21 @@ if [ $# -eq 0 ] ; then
   exit 1
 fi
 
-if [ -z $MONGO_URI ] ; then
+if [ -z "$MONGO_URI" ] ; then
   echo "Missing Mongo URI"
   exit 1
 fi
 
 hostname="$1"
 
-docker run --rm --mount type=bind,source=/var/run/docker.sock,destination=/var/run/docker.sock \
-  -e HOSTNAME="$hostname"                                                                      \
-  -e MONGO_URI="$MONGO_URI"                                                                    \
-  -e PYTHONUNBUFFERED=1                                                                        \
-  -d                                                                                           \
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock      \
+  -e HOSTNAME="$hostname"                                         \
+  -e MONGO_URI="$MONGO_URI"                                       \
+  -e PYTHONUNBUFFERED=1                                           \
+  -d                                                              \
   event-logger:latest
 
 # # #
 # #
 #
+
